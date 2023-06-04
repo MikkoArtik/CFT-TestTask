61802f7d4a8ef71aca55fccb21befee20f33210c
Michael <mihail.tchernov@yandex.ru>
2023-06-04 22:03:12 +0300

    Fixed pydantic models

--- auth-service/auth_app/models.py
+++ auth-service/auth_app/models.py
@@ -4,6 +4,7 @@ This module contains models with database fields.
 
 """
 
+from datetime import MINYEAR, date, datetime
 from typing import List
 
 from pydantic import BaseModel, Field
@@ -13,8 +14,30 @@ __all__ = [
     'User',
 ]
 
+DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+DATE_FORMAT = '%Y-%m-%d'
 
-class Response(BaseModel):
+
+def format_datetime(value: datetime) -> str:
+    return value.strftime(DATETIME_FORMAT)
+
+
+def format_date(value: date) -> str:
+    return value.strftime(DATE_FORMAT)
+
+
+class CustomBaseModel(BaseModel):
+    class Config:
+        """Model configuration."""
+
+        allow_population_by_field_name = True
+        json_encoders = {
+            date: format_date,
+            datetime: format_datetime
+        }
+
+
+class Response(CustomBaseModel):
     """Model with API response params.
 
     Args:
@@ -27,11 +50,6 @@ class Response(BaseModel):
     message: str = Field(alias='Message', default='All good.')
     data: List = Field(alias='Data', default=[])
 
-    class Config:
-        """Model configuration."""
-
-        allow_population_by_field_name = True
-
 
 class User(BaseModel):
     """Model with user information.
@@ -41,5 +59,37 @@ class User(BaseModel):
         password: user password
 
     """
+    name: str = Field(alias='Name', min_length=5, max_length=50)
     login: str = Field(alias='Login', min_length=5, max_length=20)
     password: str = Field(alias='Password', min_length=1, max_length=10)
+
+
+class Token(CustomBaseModel):
+    """Model with token information.
+
+    Args:
+        token: user login
+        expires: user password
+
+    """
+    token: str = Field(alias='AccessToken', default='')
+    expires: datetime = Field(
+        alias='Expires',
+        default=datetime(year=MINYEAR, month=1, day=1)
+    )
+
+    @property
+    def is_valid(self) -> bool:
+        return datetime.now() < self.expires
+
+
+class ActiveUser(BaseModel):
+    user_id: int = Field(alias='UserID')
+    name: str = Field(alias='Name')
+
+
+class SalaryInfo(BaseModel):
+    user_id: int = Field(alias='UserID')
+    name: str = Field(alias='Name')
+    salary: int = Field(alias='Salary')
+    target_date: date = Field(alias='TargetDate')
