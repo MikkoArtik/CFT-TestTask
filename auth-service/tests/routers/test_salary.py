704b5bfa12ad2c23f83c340e3372e6ef5789bbcd
Michael <mihail.tchernov@yandex.ru>
2023-06-07 11:06:26 +0300

    Fixed test for salary route

--- auth-service/tests/routers/test_salary.py
+++ auth-service/tests/routers/test_salary.py
@@ -9,8 +9,9 @@ from auth_app import models
 from auth_app.app import app
 from auth_app.dbase.dal.salary import SalaryDAL
 from auth_app.dbase.dal.token_ import EXPIRES_SIZE, TokenDAL
-from auth_app.dbase.dal.user import UserDAL, generate_random_string
+from auth_app.dbase.dal.user import UserDAL
 from auth_app.models import DATETIME_FORMAT
+from tests.dbase.dal.helpers import create_test_user
 
 
 class TestSalaryRoute:
@@ -32,26 +33,25 @@ class TestSalaryRoute:
     @pytest.mark.freeze_time('2023-01-01')
     @pytest.mark.asyncio
     async def test_expired_token(self, get_dbase_session, freezer):
-        table = UserDAL(session=get_dbase_session)
-        user = models.User(
-            name=generate_random_string(10),
-            login='test-' + generate_random_string(5),
-            password=generate_random_string(5)
-        )
-        await table.add(user=user)
-        user_id = await table.get_id_by_login(login=user.login)
+        user = create_test_user()
+        user_dal = UserDAL(session=get_dbase_session)
+        await user_dal.add(user=user)
 
-        table = TokenDAL(session=get_dbase_session)
-        await table.add(user_id=user_id)
+        user_id = await user_dal.get_id_by_login(login=user.login)
 
-        token_value = await table.get_by_user_id(id_=user_id)
+        token_dal = TokenDAL(session=get_dbase_session)
+        await token_dal.add(user_id=user_id)
+
+        token_value = await token_dal.get_by_user_id(id_=user_id)
 
         freezer.move_to('2023-02-01')
 
         with TestClient(app) as client:
             response = client.get(
                 url='/salary',
-                headers={'Authorization': f'Bearer {token_value.value}'}
+                headers={
+                    'Authorization': f'Bearer {token_value.value}'
+                }
             )
 
         assert_that(
@@ -67,19 +67,16 @@ class TestSalaryRoute:
     @pytest.mark.freeze_time('2023-01-01')
     @pytest.mark.asyncio
     async def test_good_token_without_salary(self, get_dbase_session, freezer):
-        table = UserDAL(session=get_dbase_session)
-        user = models.User(
-            name=generate_random_string(10),
-            login='test-' + generate_random_string(5),
-            password=generate_random_string(5)
-        )
-        await table.add(user=user)
-        user_id = await table.get_id_by_login(login=user.login)
+        user = create_test_user()
+        user_dal = UserDAL(session=get_dbase_session)
+        await user_dal.add(user=user)
 
-        table = TokenDAL(session=get_dbase_session)
-        await table.add(user_id=user_id)
+        user_id = await user_dal.get_id_by_login(login=user.login)
 
-        token_value = await table.get_by_user_id(id_=user_id)
+        token_dal = TokenDAL(session=get_dbase_session)
+        await token_dal.add(user_id=user_id)
+
+        token_value = await token_dal.get_by_user_id(id_=user_id)
 
         next_time = datetime(2023, 1, 1) + EXPIRES_SIZE + timedelta(
             seconds=-30
@@ -90,7 +87,9 @@ class TestSalaryRoute:
         with TestClient(app) as client:
             response = client.get(
                 url='/salary',
-                headers={'Authorization': f'Bearer {token_value.value}'}
+                headers={
+                    'Authorization': f'Bearer {token_value.value}'
+                }
             )
 
         assert_that(
@@ -106,23 +105,20 @@ class TestSalaryRoute:
     @pytest.mark.freeze_time('2023-01-01')
     @pytest.mark.asyncio
     async def test_good_token_with_salary(self, get_dbase_session, freezer):
-        table = UserDAL(session=get_dbase_session)
-        user = models.User(
-            name=generate_random_string(10),
-            login='test-' + generate_random_string(5),
-            password=generate_random_string(5)
-        )
-        await table.add(user=user)
-        user_id = await table.get_id_by_login(login=user.login)
+        user = create_test_user()
+        user_dal = UserDAL(session=get_dbase_session)
+        await user_dal.add(user=user)
 
-        table = SalaryDAL(session=get_dbase_session)
+        user_id = await user_dal.get_id_by_login(login=user.login)
+
+        salary_dal = SalaryDAL(session=get_dbase_session)
         expected_salary_info = models.SalaryInfo(
             user_id=user_id,
             name=user.name,
             value=123456,
             target_date=datetime.now().date()
         )
-        await table.add(salary_info=expected_salary_info)
+        await salary_dal.add(salary_info=expected_salary_info)
 
         request_data = {
             'username': user.login,
@@ -136,10 +132,10 @@ class TestSalaryRoute:
             matcher=equal_to(200)
         )
 
-        table = TokenDAL(session=get_dbase_session)
-        await table.add(user_id=user_id)
+        token_dal = TokenDAL(session=get_dbase_session)
+        await token_dal.add(user_id=user_id)
 
-        token_value = await table.get_by_user_id(id_=user_id)
+        token_value = await token_dal.get_by_user_id(id_=user_id)
 
         next_time = datetime(2023, 1, 1) + EXPIRES_SIZE + timedelta(
             seconds=-30
