cb4122c0027be6fd040eb64a576b796a4edf29f3
Michael <mihail.tchernov@yandex.ru>
2023-06-07 10:19:23 +0300

    Changes output types for helper methods

--- auth-service/tests/dbase/dal/helpers.py
+++ auth-service/tests/dbase/dal/helpers.py
@@ -1,23 +1,23 @@
-from typing import List, Tuple
+from typing import List
 
 from auth_app.dbase.dal.user import (HASH_SALT_DELIMITER,
                                      generate_random_string,
                                      get_hashed_password)
 
 
-def generate_valid_passwords(count: int = 10) -> List[Tuple[str, str, str]]:
+def generate_valid_passwords(count: int = 10) -> List[List[str]]:
     passwords = []
     for _ in range(count):
         password = generate_random_string(length=10)
         salt = generate_random_string(length=5)
         hashed_password = get_hashed_password(password=password, salt=salt)
         passwords.append(
-            (password, salt, HASH_SALT_DELIMITER.join([hashed_password, salt]))
+            [password, salt, HASH_SALT_DELIMITER.join([hashed_password, salt])]
         )
     return passwords
 
 
-def generate_invalid_passwords(count: int = 10) -> List[Tuple[str, str, str]]:
+def generate_invalid_passwords(count: int = 10) -> List[List[str]]:
     passwords = []
     for _ in range(count):
         password = generate_random_string(length=10)
@@ -27,6 +27,6 @@ def generate_invalid_passwords(count: int = 10) -> List[Tuple[str, str, str]]:
             salt=salt + 'q'
         )
         passwords.append(
-            (password, salt, HASH_SALT_DELIMITER.join([hashed_password, salt]))
+            [password, salt, HASH_SALT_DELIMITER.join([hashed_password, salt])]
         )
     return passwords
