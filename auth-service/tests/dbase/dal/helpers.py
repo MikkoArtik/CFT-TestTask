44dd752368750f01e3866b757d294307dbc1a041
Michael <mihail.tchernov@yandex.ru>
2023-06-07 10:20:07 +0300

    Added method for generating test user

--- auth-service/tests/dbase/dal/helpers.py
+++ auth-service/tests/dbase/dal/helpers.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from auth_app import models
 from auth_app.dbase.dal.user import (HASH_SALT_DELIMITER,
                                      generate_random_string,
                                      get_hashed_password)
@@ -30,3 +31,11 @@ def generate_invalid_passwords(count: int = 10) -> List[List[str]]:
             [password, salt, HASH_SALT_DELIMITER.join([hashed_password, salt])]
         )
     return passwords
+
+
+def create_test_user() -> models.User:
+    return models.User(
+        name=generate_random_string(length=10),
+        login='test-' + generate_random_string(length=5),
+        password=generate_random_string(length=7)
+    )
