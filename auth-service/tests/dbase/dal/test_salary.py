2aea6941af3227f0a6e233cac9bfa39cf5f92cb2
Michael <mihail.tchernov@yandex.ru>
2023-06-07 10:23:51 +0300

    Fixed test for SalaryDAL

--- auth-service/tests/dbase/dal/test_salary.py
+++ auth-service/tests/dbase/dal/test_salary.py
@@ -1,8 +1,11 @@
+import random
 from datetime import datetime
+from typing import Union
 
 import pytest
-from hamcrest import assert_that, equal_to
+from hamcrest import assert_that, equal_to, is_
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from auth_app import models
 from auth_app.dbase import orm
@@ -11,20 +14,59 @@ from auth_app.dbase.dal.salary import SalaryDAL
 
 class TestSalaryDAL:
 
+    @staticmethod
+    async def get_random_existing_user(
+            session: AsyncSession) -> Union[orm.User, None]:
+        query = select(orm.User)
+        records = await session.execute(query)
+        users = records.scalars().all()
+        if not users:
+            return
+        return random.choice(users)
+
+    @pytest.mark.asyncio
+    async def test_add(self, get_dbase_session):
+        user = await self.get_random_existing_user(session=get_dbase_session)
+        salary_info = models.SalaryInfo(
+            user_id=user.id_,
+            name=user.name,
+            value=123456,
+            target_date=datetime.now().date()
+        )
+
+        salary_dal = SalaryDAL(get_dbase_session)
+        is_success = await salary_dal.add(salary_info=salary_info)
+
+        assert_that(
+            actual_or_assertion=is_success,
+            matcher=is_(True)
+        )
+
+        query = select(orm.Salary).where(orm.Salary.user_id == user.id_)
+        salary_orm: orm.Salary = (
+            await get_dbase_session.execute(query)
+        ).scalar()
+
+        assert_that(
+            actual_or_assertion=salary_orm.value,
+            matcher=equal_to(salary_info.value)
+        )
+        assert_that(
+            actual_or_assertion=salary_orm.target_date,
+            matcher=equal_to(salary_info.target_date)
+        )
+
     @pytest.mark.asyncio
     @pytest.mark.parametrize('is_exist', [True, False])
     async def test_get_by_user_id(self, get_dbase_session, is_exist: bool):
         if is_exist:
-            query = select(orm.Salary).limit(1)
-            record = await get_dbase_session.execute(query)
-            salary_orm = record.scalar()
-            user_id: int = salary_orm.user_id
+            query = select(orm.Salary)
+            records = (await get_dbase_session.execute(query)).scalars().all()
+            salary_orm = random.choice(records)
 
-            query = select(orm.User.name).where(
-                orm.User.id_ == user_id
-            )
-            record = await get_dbase_session.execute(query)
-            name = record.scalar()
+            user_id: int = salary_orm.user_id
+            query = select(orm.User.name).where(orm.User.id_ == user_id)
+            name = (await get_dbase_session.execute(query)).scalar()
 
             expected_salary = models.SalaryInfo(
                 user_id=user_id,
@@ -42,25 +84,4 @@ class TestSalaryDAL:
         assert_that(
             actual_or_assertion=salary_info,
             matcher=equal_to(expected_salary)
-        )
-
-    @pytest.mark.asyncio
-    async def test_add(self, get_dbase_session):
-        query = select(orm.User).limit(1)
-        record = await get_dbase_session.execute(query)
-        user_orm = record.scalar()
-
-        salary_info = models.SalaryInfo(
-            user_id=user_orm.id_,
-            name=user_orm.name,
-            value=123456,
-            target_date=datetime.now().date()
-        )
-        table_dal = SalaryDAL(get_dbase_session)
-        await table_dal.add(salary_info=salary_info)
-
-        actual_salary_info = await table_dal.get_by_user_id(id_=user_orm.id_)
-        assert_that(
-            actual_or_assertion=actual_salary_info,
-            matcher=equal_to(salary_info)
         )
